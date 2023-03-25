0da464f772fe2c9320ac234ac7d0e0eb455c2edd
adarshkumar714 <123639529+adarshkumar714@users.noreply.github.com>
2023-03-25 12:45:53 +0530

    created Polymorphism.java

---
+++ Polymorphism.java
@@ -0,0 +1,31 @@
+public class Polymorphism{
+    public static void main(String args[]){
+
+        // creating object of Robot class
+        Robot r1 = new Robot();
+
+        r1.name = "abhi";
+        r1.speed = 120;
+
+        r1.printInfo(r1.speed);
+        r1.printInfo(r1.name);
+        r1.printInfo(r1.speed, r1.name);
+        
+    }
+}
+
+/* --- POLYMORPHISM --- */
+class Robot{
+    String name;
+    int speed;
+    
+    void printInfo(int speed){
+        System.out.println(this.speed);
+    }
+    void printInfo(String name){
+        System.out.println(this.name);
+    }
+    void printInfo(int speed, String name){
+        System.out.println(this.speed + " " + this.name);
+    }
+}
