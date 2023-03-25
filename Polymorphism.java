88334bb2b150082a653c0b16613c4072c23a9d9a
adarshkumar714 <123639529+adarshkumar714@users.noreply.github.com>
2023-03-25 12:46:55 +0530

    updated Polymorphism.java

--- Polymorphism.java
+++ Polymorphism.java
@@ -10,21 +10,24 @@ public class Polymorphism{
         r1.printInfo(r1.speed);
         r1.printInfo(r1.name);
         r1.printInfo(r1.speed, r1.name);
-        
+
     }
 }
 
 /* --- POLYMORPHISM --- */
 class Robot{
+    
     String name;
     int speed;
     
     void printInfo(int speed){
         System.out.println(this.speed);
     }
+
     void printInfo(String name){
         System.out.println(this.name);
     }
+
     void printInfo(int speed, String name){
         System.out.println(this.speed + " " + this.name);
     }
