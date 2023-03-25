d00ea5ea0dd2468e9faac382ae9cd126d0df330b
adarshkumar714 <123639529+adarshkumar714@users.noreply.github.com>
2023-03-25 12:50:54 +0530

    updated Classes_Objects_and_Constructors.java

--- Classes_Objects_and_Constructors.java
+++ Classes_Objects_and_Constructors.java
@@ -1,12 +1,16 @@
 public class Classes_Objects_and_Constructors{
     public static void main(String args[]){
-        System.out.println("-----------------classes, objects and constructors");
-        // classes, objects, constructors and Encapsulations
+
+        // using static variable
         Car.name = "this is the static keyword";
+
+        // creating object of Car class
         Car c1 = new Car();
         c1.brand = "BMW";
         c1.prise();
         c1.name();
+
+        // creating 2nd object of Car class
         Car c2 = new Car();
         c2.name();
     }
@@ -14,16 +18,23 @@ public class Classes_Objects_and_Constructors{
 
 // classes, objects and constructors
 class Car{
+
+    // variable with static keyword
     static String name;
+
     int prise;
     String color;
     String brand;
+
+    // constructor of Car class
     Car(){
         System.out.println("this is the constructor of Car class");
     }
+
     void name(){
         System.out.println(name);
     }
+
     void prise(){
         switch(brand){
             case "audi":
